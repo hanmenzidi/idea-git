e1a6566d15e2aca51314dc7402d355c2f34d5277
lihan <lihan997@163.com>
2021-09-23 19:32:44 +0800

    第二次修改

--- src/main/java/com/study/lihan/git/demo/DemoApplication.java
+++ src/main/java/com/study/lihan/git/demo/DemoApplication.java
@@ -9,7 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("修改");
+		System.out.println("修改1");
+		System.out.println("修改2");
 	}
 
 }
